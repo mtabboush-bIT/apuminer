9dc332ebb029635f158c89ea60a5423ef75aa678
Mouaz Tabboush <mouaz.tabboush@bridging-it.de>
2023-07-04 13:43:52 +0200

    auto commit

--- runContainer.ps1
+++ runContainer.ps1
@@ -1,3 +1,7 @@
+docker image rm $(docker image ls -q)
+docker container rm $(doocker container ls -q)
+
 docker build . -t apuminer-image
 
 docker run -d -p 5000:5000 --name apuminer-container apuminer-image
+
